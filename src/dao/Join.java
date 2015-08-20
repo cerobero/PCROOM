ea1234ee434f5160fe50cf61c3aecf94eef2c5f1
minji0211 <dnfrosms@naver.com>
2015-08-20 16:26:41 +0900

    Login, Regist Frame

---
+++ src/dao/Join.java
@@ -0,0 +1,60 @@
+package dao;
+
+import java.sql.Date;
+
+public class Join {
+	private String userId;
+	private String password;
+	private String cellPhone;
+	private String birth;
+
+////////////////////////////////////////////////
+	public Join(){
+		
+	}
+
+	public Join(String userId, String password, String cellPhone, String birth) {
+		super();
+		this.userId = userId;
+		this.password = password;
+		this.cellPhone = cellPhone;
+		this.birth = birth;
+	}
+
+////////////////////////////////////////////////
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+
+	public String  getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String  birth) {
+		this.birth = birth;
+	}
+	
+	
+
+}
---
+++ src/ui/client/FrameJoin.java
@@ -0,0 +1,126 @@
+package ui.client;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import dao.Join;
+import dao.JoinDao;
+
+public class FrameJoin extends JFrame {
+	private JTextField textfield_id; 
+	private JTextField textfield_pw; 
+	private JTextField textfield_phone;
+	private JTextField textfield_birth;
+	private JButton button_join; 
+	private JButton button_cancel; 
+////////////////////////////////////////////////////////////////////	
+	JoinDao dao = new JoinDao();
+	Join newJoin;
+	
+////////////////////////////////////////////////////////////////////
+	public FrameJoin() {
+		
+		setLayout(null);
+
+		JLabel label_title = new JLabel("회원가입");
+		label_title.setFont(new Font("맑은 고딕", Font.PLAIN, 30));
+		label_title.setBounds(80, 30, 200, 50);
+		add(label_title);
+
+		JLabel label_id = new JLabel("ID");
+		label_id.setBounds(40, 100, 50, 20);
+		add(label_id);
+
+		textfield_id = new JTextField();
+		textfield_id.setBounds(130, 100, 100, 20);
+		add(textfield_id);
+
+		JLabel label_pw = new JLabel("PW");
+		label_pw.setBounds(40, 130, 50, 20);
+		add(label_pw);
+
+		textfield_pw = new JTextField();
+		textfield_pw.setBounds(130, 130, 100, 20);
+		add(textfield_pw);
+
+		JLabel label_phone = new JLabel("핸드폰");
+		label_phone.setBounds(40, 160, 50, 20);
+		add(label_phone);
+
+		textfield_phone = new JTextField();
+		textfield_phone.setBounds(130, 160, 100, 20);
+		add(textfield_phone);
+
+		JLabel label_birth = new JLabel("생년월일");
+		label_birth.setBounds(40, 190, 80, 20);
+		add(label_birth);
+
+		textfield_birth = new JTextField();
+		textfield_birth.setBounds(130, 190, 100, 20);
+		add(textfield_birth);
+
+		button_join = new JButton("회원가입");
+		button_join.setBounds(30, 250, 100, 30);
+		add(button_join);
+		
+		
+		button_cancel = new JButton("취소");
+		button_cancel.setBounds(150, 250, 100, 30);
+		add(button_cancel);
+		
+		//이벤트
+		MyListener my = new MyListener();
+		button_join.addActionListener(my);
+		button_cancel.addActionListener(my);
+		
+		
+		setSize(300, 350);
+		setTitle("회원가입");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+
+	}
+///////////////////////////////////////////////////////
+	class MyListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == button_join){
+			
+			newJoin = new Join();
+			newJoin.setUserId(textfield_id.getText());
+			newJoin.setPassword(textfield_pw.getText());
+			newJoin.setCellPhone(textfield_birth.getText());
+			newJoin.setBirth(textfield_birth.getText());
+			
+			dao.insert(newJoin);
+			
+			textfield_id.setText("");
+			textfield_pw.setText("");
+			textfield_phone.setText("");
+			textfield_birth.setText("");
+			
+			}else if(e.getSource() == button_cancel){
+				textfield_id.setText("");
+				textfield_pw.setText("");
+				textfield_phone.setText("");
+				textfield_birth.setText("");
+			}
+		}
+		
+	}
+	
+///////////////////////////////////////////////////////	
+	
+	
+	public static void main(String[] args) {
+		new FrameJoin();
+	}
+
+}
