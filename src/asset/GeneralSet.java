e0b6371e999c6df5b5d1ed2d57e2ff7ce2ead07d
GEONWOO-CHO <msrjsdn@gmail.com>
2015-08-24 15:34:03 +0900

    src/asset/GeneralSet.java : 디버깅용 프린트 메소드 추가 print(String msg); 형태로 사용, 출력시 msg (FileName:FileNumber) 형식으로 출력되어 이클립스에서 콘솔로그 클릭시 해당 소스 라인으로 이동하여 손쉽게 로깅 확인 및 디버깅 가능

--- src/asset/GeneralSet.java
+++ src/asset/GeneralSet.java
@@ -9,4 +9,10 @@ public class GeneralSet
 	public static final String FOOD_DB_URL = "jdbc:mysl//localhost:3306/product_db";
 	public static final String FOOD_DB_ID = "root";
 	public static final String FOOD_DB_PW = "hanbit";
+	
+	public static void print(String msg)
+	{
+		StackTraceElement elem = new Throwable().getStackTrace()[1];
+		System.out.println(msg + " (" + elem.getFileName() + ":" + elem.getLineNumber() + ")");
+	}
 }
